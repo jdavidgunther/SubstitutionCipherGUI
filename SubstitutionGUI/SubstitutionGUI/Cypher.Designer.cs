c85db25ec7fbeb97566a8525f365ddb05e06c0a9
Jack Gunther <369798@colum.edu>
2020-11-23 16:43:46 -0600

    Added Substitution Cypher GUI

---
+++ SubstitutionGUI/SubstitutionGUI/Cypher.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SubstitutionGUI
+{
+    partial class Cypher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Encode = new System.Windows.Forms.Button();
+            this.Decode = new System.Windows.Forms.Button();
+            this.Codes = new System.Windows.Forms.TextBox();
+            this.Input = new System.Windows.Forms.Label();
+            this.Output = new System.Windows.Forms.Label();
+            this.codeOutput = new System.Windows.Forms.RichTextBox();
+            this.Invalid = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Encode
+            // 
+            this.Encode.Location = new System.Drawing.Point(147, 107);
+            this.Encode.Name = "Encode";
+            this.Encode.Size = new System.Drawing.Size(127, 43);
+            this.Encode.TabIndex = 0;
+            this.Encode.Text = "Encode";
+            this.Encode.UseVisualStyleBackColor = true;
+            this.Encode.Click += new System.EventHandler(this.Encode_Click);
+            // 
+            // Decode
+            // 
+            this.Decode.Location = new System.Drawing.Point(423, 107);
+            this.Decode.Name = "Decode";
+            this.Decode.Size = new System.Drawing.Size(127, 43);
+            this.Decode.TabIndex = 1;
+            this.Decode.Text = "Decode";
+            this.Decode.UseVisualStyleBackColor = true;
+            this.Decode.Click += new System.EventHandler(this.Decode_Click);
+            // 
+            // Codes
+            // 
+            this.Codes.Location = new System.Drawing.Point(147, 46);
+            this.Codes.Name = "Codes";
+            this.Codes.Size = new System.Drawing.Size(403, 20);
+            this.Codes.TabIndex = 2;
+            // 
+            // Input
+            // 
+            this.Input.AutoSize = true;
+            this.Input.Location = new System.Drawing.Point(336, 20);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(31, 13);
+            this.Input.TabIndex = 3;
+            this.Input.Text = "Input";
+            // 
+            // Output
+            // 
+            this.Output.AutoSize = true;
+            this.Output.Location = new System.Drawing.Point(328, 176);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(39, 13);
+            this.Output.TabIndex = 4;
+            this.Output.Text = "Output";
+            // 
+            // codeOutput
+            // 
+            this.codeOutput.Location = new System.Drawing.Point(147, 213);
+            this.codeOutput.Name = "codeOutput";
+            this.codeOutput.Size = new System.Drawing.Size(403, 26);
+            this.codeOutput.TabIndex = 5;
+            this.codeOutput.Text = "";
+            // 
+            // Invalid
+            // 
+            this.Invalid.Location = new System.Drawing.Point(319, 82);
+            this.Invalid.Name = "Invalid";
+            this.Invalid.Size = new System.Drawing.Size(61, 21);
+            this.Invalid.TabIndex = 6;
+            this.Invalid.Text = "";
+            // 
+            // Cypher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 261);
+            this.Controls.Add(this.Invalid);
+            this.Controls.Add(this.codeOutput);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.Codes);
+            this.Controls.Add(this.Decode);
+            this.Controls.Add(this.Encode);
+            this.Name = "Cypher";
+            this.Text = "Cypher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Encode;
+        private System.Windows.Forms.Button Decode;
+        private System.Windows.Forms.TextBox Codes;
+        private System.Windows.Forms.Label Input;
+        private System.Windows.Forms.Label Output;
+        private System.Windows.Forms.RichTextBox codeOutput;
+        private System.Windows.Forms.RichTextBox Invalid;
+    }
+}
---
+++ SubstitutionGUI/SubstitutionGUI/Cypher.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SubstitutionGUI
+{
+    public partial class Cypher : Form
+    {
+        public Cypher()
+        {
+            InitializeComponent();
+        }
+
+        private void Encode_Click(object sender, EventArgs e)
+        {
+            string code;            
+            string alphabet = " abcdefghijklmnopqrstuvwxyz0123456789";
+            char[] alphabetCharArray = alphabet.ToCharArray();
+
+            code = Codes.Text;
+
+            char[] inputArray = code.ToCharArray();
+            int shift = 5;
+            string output = "";
+            bool isValid = true;
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                int index = Array.IndexOf(alphabetCharArray, inputArray[i]);
+                if (index == -1)
+                {
+                    isValid = false;
+                    break;
+                }
+                index = index + shift;
+                if (index >= alphabetCharArray.Length)
+                {
+                    index = index - alphabetCharArray.Length;
+                }                
+                //Console.WriteLine(index);
+                //Use an if statement to see if the shifted index is higher than the total array length.
+
+
+                //Console.WriteLine($"{inputArray[i]}: new index: {index}");
+                //Get the shifted characther from array using the your new index number.
+                //Console.WriteLine($"{inputArray[i]} becomes {alphabetCharArray[index]}");
+                //Combine all the new characthers and create the new sentence
+                output += alphabetCharArray[index];                               
+            }
+            if (isValid)
+            {
+                codeOutput.Text = output;
+                Invalid.Text = "Valid";
+            }
+            else
+            {
+                Invalid.Text = "Invalid";
+            }
+
+        }
+
+        private void Decode_Click(object sender, EventArgs e)
+        {
+            string code;
+            string alphabet = " abcdefghijklmnopqrstuvwxyz0123456789";
+            char[] alphabetCharArray = alphabet.ToCharArray();
+
+            code = Codes.Text;
+
+            char[] inputArray = code.ToCharArray();
+            int shift = 5;
+            string output = "";
+            bool isValid = true;
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                int index = Array.IndexOf(alphabetCharArray, inputArray[i]);
+                if (index == -1)
+                {
+                    isValid = false;
+                    break;
+                }
+                index = index - shift;
+                if (index < 0)
+                {
+                    index = index + alphabetCharArray.Length;
+                }
+                //Console.WriteLine(index);
+                //Use an if statement to see if the shifted index is higher than the total array length.
+
+
+                //Console.WriteLine($"{inputArray[i]}: new index: {index}");
+                //Get the shifted characther from array using the your new index number.
+                //Console.WriteLine($"{inputArray[i]} becomes {alphabetCharArray[index]}");
+                //Combine all the new characthers and create the new sentence
+                output += alphabetCharArray[index];
+            }
+            if (isValid)
+            {
+                codeOutput.Text = output;
+                Invalid.Text = "Valid";
+            }
+            else
+            {
+                Invalid.Text = "Invalid";
+            }
+
+        }
+    }
+}
